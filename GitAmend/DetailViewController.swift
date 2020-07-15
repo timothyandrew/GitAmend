bb9d33ed917cd9f4b7dc7cb9147210bc6a404025
Timothy Andrew <mail@timothyandrew.net>
2020-07-15 22:24:27 +0530

    Initial Commit

---
+++ GitAmend/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  GitAmend
+//
+//  Created by Timothy Andrew on 15/07/20.
+//  Copyright © 2020 Timothy Andrew. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
