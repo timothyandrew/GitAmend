04bdf3beb7c01ea5c4e838a700210e8ec05801bb
Timothy Andrew <mail@timothyandrew.net>
2020-07-19 21:39:54 +0530

    allow json

--- GitAmend/GithubAPI/GithubAPITree.swift
+++ GitAmend/GithubAPI/GithubAPITree.swift
@@ -11,9 +11,15 @@ import UIKit
 class GithubAPITree: NSObject, Decodable {
     let tree: [GithubAPIFile]
     let sha: String
+    let allowedExtensions: Set = ["md", "json"]
+    
+    enum CodingKeys: String, CodingKey {
+        case tree
+        case sha
+    }
 
     func files() -> [GithubAPIFile] {
-        self.tree.filter { $0.type == "blob" && $0.fileExt() == "md" }
+        self.tree.filter { $0.type == "blob" && allowedExtensions.contains($0.fileExt()) }
     }
     
     static func create(baseSha: String, blobSha: String, file: GithubAPIFile, repo: String, _ completion: @escaping (_ tree: GithubAPITree) -> Void) {
